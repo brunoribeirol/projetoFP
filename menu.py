03d58fb92dc4b9c8ec7994ef96228c9f9aeb4301
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-05-24 00:12:11 -0300

    Testando código com o menu
    Código estava funcionando, mas o arquivo estava salvo no lugar errado do meu pc e agora está dando erro nas linhas 28 e 94

--- menu.py
+++ menu.py
@@ -5,41 +5,117 @@
 # Funcionalidade Extra - FALTA
 # READ.me - FALTA
 
-# USAR ISSO PARA O CÓDIGO FINAL
 
-# Função criada para calcular o saldo do usuário
-''''
-def calculandoSaldo():
-    saldo = 0
-    for transacao in planilha:
-        preco = transacao.get("valor") #???
-        if preco:
-            saldo += float(preco) #??????
-
-    #Verificando se o saldo é positivo ou negativo (maior ou menor que zero), e dependendo se ele for positivo ou negativo irá retornar uma cor diferente
-    if saldo >= 0:
-        saldoFinal = "\033[92mR${:.2f}\033[0m".format(saldo) #Verde
-    else:
-        saldoFinal = "\033[91mR${:.2f}\033[0m".format(saldo)  #Vermelho
-    
-    return saldoFinal
-'''
-
-#Criação da funcionalidade que deleta tudo que tem no arquivo .csv
-'''
-def apagaTudo():
-'''
-
-
-# Carregar as transações existentes
-planilha = ler()
-# transactions = read_transactions() #TA ERRADO ler()
+# ***********************LEMBRAR DE MUDAR OS PATHS
+# CADA TRANSAÇÃO É UM DICIONARIO, DENTRO DE UMA LISTA DE TRASACOES
+planilha = [{"nome": "Nome", "categoria": "Categoria", "valor": "Valor"}]
+# NOVA TRANSACAO (EX)
+transacao = {}
 
 import os
 
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
+
+
+#######################funcao read
+def ler():
+    global planilha
+    try:
+        with open(
+            "/Users/brunoribeiro/Documents/GitHub/projetoFP/transacoes.csv", "r"
+        ) as file:
+            linhas = file.readlines()  # TODAS AS LINHAS VIRAM UM ITEM DE UMA LISTA
+            for linha in linhas[1:]:  # ????  1 NAO PEGA O "TITULO" DO ARQUIVO
+                itens = linha.strip().split(
+                    ","
+                )  # ITENS = LISTA QUE TEM OS DADOS DE CADA TRANSAÇÃO
+                transacao = {
+                    "nome": itens[0],
+                    "categoria": itens[1],
+                    "valor": float(itens[2]),
+                }  # PEGA CADA TRANSACAO E ADICIONA NO DICIONARIO
+                planilha.append(transacao)
+    except FileNotFoundError:
+        pass  # PESQUISAR SOBRE (BREAK SO FUNCIONA DENTRO DO WHILE, COMO É UMA FUNCAO QUE VAI SER CHAMADA NAS OUTRAS, NAO PRECISA DO WHILE)
+
+    return planilha
+
+
+##############################funcao write
+def armazena():  # TRANSFORMAR O DICIONARIO TRANSACAO EM UM STRING LINHA
+    with open(
+        "/Users/brunoribeiro/Documents/GitHub/projetoFP/transacoes.csv",
+        "w+",
+        encoding="utf-8",
+    ) as file:
+        for i, transacao in enumerate(planilha):  # i é idice, nao usei
+            itens = []
+            for dados in transacao.values():
+                itens.append(str(dados))
+                linha = ",".join(itens)
+            file.write(linha + "\n")
+
+
+#############################funcao add
+def adicao():
+    print("***Adicionar nova transação***")
+    with open(
+        "//Users/brunoribeiro/Documents/GitHub/projetoFP/transacoes.csv", "w"
+    ) as file:
+        nome = str(input("Nome: "))
+        categoria = str(input("Categoria: ")).title()
+        valor = str(input("Valor: R$"))
+        transacao = {"nome": nome, "categoria": categoria, "valor": valor}
+        planilha.append(transacao)
+        armazena()
+
+
+#############################funcao delete
+def delete():
+    print("***Apagar transação existente***")
+    with open(
+        "/Users/brunoribeiro/Documents/GitHub/projetoFP/transacoes.csv", "r"
+    ) as file:
+        nome = str(input("Nome: "))
+        categoria = str(input("Categoria: ")).title()
+        valor = float(input("Valor: R$"))
+        transacao_pra_achar = {"nome": nome, "categoria": categoria, "valor": valor}
+        ler()
+        planilha.remove(transacao_pra_achar)
+        armazena()
+
+
+# USAR ISSO PARA O CÓDIGO FINAL
+
+
+# Função criada para calcular o saldo do usuário
+def calculandoSaldo():
+    saldo = 0
+    for transacao in planilha:
+        preco = transacao.get("valor")  # ???
+        if preco:
+            saldo += float(preco)  # ??????
+
+    # Verificando se o saldo é positivo ou negativo (maior ou menor que zero), e dependendo se ele for positivo ou negativo irá retornar uma cor diferente
+    if saldo >= 0:
+        saldoFinal = "\033[92mR${:.2f}\033[0m".format(saldo)  # Verde
+    else:
+        saldoFinal = "\033[91mR${:.2f}\033[0m".format(saldo)  # Vermelho
+
+    return saldoFinal
+
+
+# Criação da funcionalidade que deleta tudo que tem no arquivo .csv
+"""
+def apagaTudo():
+"""
+
+
+# Carregar as transações existentes
+planilha = ler()
+# transactions = read_transactions() #TA ERRADO ler()
 
 
 # Criação da função menu para diminuir o bloco de código no loop principal
@@ -67,36 +143,18 @@ while True:
 
     if acao == "1":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # adicao()
-        input("\nPressione Enter para continuar...")
-
-    elif acao == "2":
-        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # extrato()
-        # list_all_transactions(transactions) VER ESSA
-        input("\nPressione Enter para continuar...")
-
-    elif acao == "3":
-        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # atualizacao()
-        # list_all_transactions(transactions)
+        adicao()
         input("\nPressione Enter para continuar...")
 
     elif acao == "4":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # delete()
+        delete()
         # list_all_transactions(transactions)
-        input("\nPressione Enter para continuar...")
-
-    elif acao == "5":
-        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # apagarTudo()
-        # FALTA VER QUAL EXTRA VOU COLOCAR
         input("\nPressione Enter para continuar...")
 
     elif acao == "6":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
-        # print(f"Saldo Atual: {calculandoSaldo()}")
+        print(f"Saldo Atual: {calculandoSaldo()}")
         input("\nPressione Enter para continuar...")
 
     elif acao == "7":
@@ -108,7 +166,31 @@ while True:
         input("\nPressione Enter para continuar...")
 
 
-"""for transacao in planilha:
+"""
+    elif acao == "2":
+        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
+        extrato()
+        # list_all_transactions(transactions) VER ESSA
+        input("\nPressione Enter para continuar...")
+    
+    elif acao == "3":
+        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
+        atualizacao()
+        # list_all_transactions(transactions)
+        input("\nPressione Enter para continuar...")
+
+
+        
+    elif acao == "5":
+        # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
+        apagarTudo()
+        # FALTA VER QUAL EXTRA VOU COLOCAR
+        input("\nPressione Enter para continuar...")
+"""
+
+
+"""
+for transacao in planilha:
     for i in transacao.keys():
         del transacao[i]
 """
