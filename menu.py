cc2f403a48e2352356c0c241eb838b829019878a
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-05-24 12:01:23 -0300

    Ajeitando as cores do menu

--- menu.py
+++ menu.py
@@ -1,13 +1,17 @@
 import os
 
+
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def menu():
+    print("\033[0m")
+
     print("\033[1;3m--------------------------------")
     print("\033[1;3m         MENU PRINCIPAL")
     print("\033[1;3m--------------------------------")
-    
+
     print("\033[0;34m 1. Adicionar Transação")
     print("\033[0;35m 2. Listar Todas as Transações")
     print("\033[0;36m 3. Atualização de uma Transação")
@@ -17,51 +21,54 @@ def menu():
     print("\033[0;31m 7. Sair do Programa")
     print("\033[0m")
 
+
 while True:
     clear_screen()
 
     menu()
 
-    acao = input("\nEscolha uma opção: ")
+    acao = input("\033[1m\nEscolha uma opção: ")
 
     if acao == "1":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # adicao()
-        input("\nPressione Enter para continuar...")
+        input("\033[0;30m\nPressione Enter para continuar...")
 
     elif acao == "2":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # extrato()
-        # list_all_transactions(transactions) VER ESSA
-        input("\nPressione Enter para continuar...")
+        # list_all_transactions(transactions) VER SE PRECISA DISSO
+        input("\033[0;30m\nPressione Enter para continuar...")
 
     elif acao == "3":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # atualizacao()
-        # list_all_transactions(transactions)
-        input("\nPressione Enter para continuar...")
+        # list_all_transactions(transactions) VER SE PRECISA DISSO
+        input("\033[0;30m\nPressione Enter para continuar...")
 
     elif acao == "4":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # delete()
-        # list_all_transactions(transactions)
-        input("\nPressione Enter para continuar...")
+        # list_all_transactions(transactions) VER SE PRECISA DISSO
+        input("\033[0;30m\nPressione Enter para continuar...")
 
     elif acao == "5":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # apagarTudo()
-        # FALTA VER QUAL EXTRA VOU COLOCAR
-        input("\nPressione Enter para continuar...")
-    
+
+        # COLOCAR O PRINT DO APAGAR TDUO FALTA !!!!!!!!!!
+
+        input("\033[0;30m\nPressione Enter para continuar...")
+
     elif acao == "6":
         # ler() Talvez não precise de nenhum desse ler() !!!!!!!!!!!!!!!!
         # print(f"Saldo Atual: {calculandoSaldo()}")
-        input("\nPressione Enter para continuar...")
+        input("\033[0;30m\nPressione Enter para continuar...")
 
     elif acao == "7":
-        print("Obrigado por utilizar o Sistema de Controle Financeiro!")
+        print("\033[1m\nObrigado por utilizar o nosso Sistema de Controle Financeiro!")
         break
 
     else:
-        print("Opção inválida. Por favor, tente novamente.")
-        input("\nPressione Enter para continuar...")
+        print("\033[91mOpção inválida. Por favor, tente novamente.")
+        input("\033[0;30m\nPressione Enter para continuar...")
